c14a97bb0de210bb27b023639db3008777327e7f
Krisna Ajie <krisnaajiep@gmail.com>
2024-07-28 19:59:33 +0700

    Menambah list

--- index.php
+++ index.php
@@ -11,6 +11,14 @@
     <h1>Hello, world!</h1>
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum perspiciatis consectetur quibusdam, necessitatibus ipsam magni placeat, aut ipsum, tenetur illum reprehenderit pariatur? Soluta facere vitae nobis perspiciatis. Nobis, minus nemo.</p>
+
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+    </ul>
 </body>
 
 </html>
